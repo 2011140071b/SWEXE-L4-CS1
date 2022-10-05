56ce6807a1d2690a3c4399530fb7c8f16d052331
2011140071b <2011140071b@ed.fuk.kindai.ac.jp>
2022-10-05 07:06:49 +0000

    ログイン機能の実装

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,18 @@
+class TopController < ApplicationController
+  def main
+    if session[:login_uid]
+      render "login"
+    else
+      render "main"
+    end
+  end
+  
+  def login
+    if params[:uid] == "kindai" && params[:pass] == "sanriko"
+      session[:login_uid] = params[:uid]
+      redirect_to "/"
+    else
+      render "error"
+    end
+  end
+end
